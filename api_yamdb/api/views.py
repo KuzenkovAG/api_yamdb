08f578c7d69170077b90fcb482911bd49f269984
Олег Синюк <oleg.sinyuk@outlook.com>
2023-04-11 19:51:46 +0700

    Commit serializer

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets, mixins
 
+from .serializers import CategorySerializer
+
 
 User = get_user_model()
 
@@ -8,4 +10,5 @@ User = get_user_model()
 class CategoriesViewSet(mixins.CreateModelMixin,
                         mixins.ListModelMixin,
                         viewsets.GenericViewSet):
-    pass
+
+    serializer_class = CategorySerializer
