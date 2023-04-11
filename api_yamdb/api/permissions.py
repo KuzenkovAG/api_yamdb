0c9ecd62746d31286bc17b3aed8bcf8bd0edf669
Alexey Kuzenkov <kuzenkovag@yandex.ru>
2023-04-11 21:28:31 +0600

    Add permission for moderator and author

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,11 +1,19 @@
-from django.contrib.auth import get_user_model
 from rest_framework import permissions
-
-User = get_user_model()
 
 
 class IsAdminPermission(permissions.BasePermission):
     """Check is admin."""
     def has_permission(self, request, view):
-        user = request.user
-        return user.is_authenticated and user.role == 'admin'
+        return request.user.role == 'admin'
+
+
+class IsModeratorPermission(permissions.BasePermission):
+    """Check is moderator."""
+    def has_permission(self, request, view):
+        return request.user.role == 'moderator'
+
+
+class IsAuthorPermission(permissions.BasePermission):
+    """Check is author."""
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
