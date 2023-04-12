8f69e9660bae9be7f0aa5b4725416ad44c3590f6
Олег Синюк <oleg.sinyuk@outlook.com>
2023-04-12 23:34:13 +0700

    Fixed models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-<<<<<<< HEAD
 from django.core.validators import MaxValueValidator, MinValueValidator
-=======
->>>>>>> 44644bd5c8d0a526fa74e55c9c07f0d00467389e
 
 
 User = get_user_model()
@@ -12,7 +9,6 @@ User = get_user_model()
 class Genre(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
-<<<<<<< HEAD
 
     def __str__(self):
         return self.name
@@ -91,33 +87,3 @@ class Review(models.Model):
 
     def __str__(self):
         return self.text
-=======
-
-    def __str__(self):
-        return self.text
-
-
-class Categories(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Titles(models.Model):
-    name = models.CharField(max_length=256)
-    year = models.DateTimeField('Год выпуска', auto_now_add=True)
-    description = models.TextField()
-    genre = models.ForeignKey(
-        Genre, on_delete=models.SET_NULL,
-        related_name='genre', blank=True, null=True
-    )
-    category = models.ForeignKey(
-        Categories, on_delete=models.SET_NULL,
-        related_name='category', blank=True, null=True
-    )
-
-    def __str__(self):
-        return self.name
->>>>>>> 44644bd5c8d0a526fa74e55c9c07f0d00467389e
