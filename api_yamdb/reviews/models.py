aae4db596055f173d4729c0dccb684087ac006bd
Олег Синюк <oleg.sinyuk@outlook.com>
2023-04-12 23:22:27 +0700

    Merge Models.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,10 +1,42 @@
-from django.db import models
-
 from django.contrib.auth import get_user_model
-
+from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 User = get_user_model()
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.DateTimeField('Год выпуска', auto_now_add=True)
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL,
+        related_name='genre', blank=True, null=True
+    )
+    category = models.ForeignKey(
+        Categories, on_delete=models.SET_NULL,
+        related_name='category', blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Review(models.Model):
@@ -36,7 +68,7 @@ class Review(models.Model):
         db_index=True
     )
     title = models.ForeignKey(
-        Title,
+        Titles,
         on_delete=models.CASCADE,
         related_name='reviews',
         verbose_name='Произведение',
